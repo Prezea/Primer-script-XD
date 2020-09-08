fc28649563e01b6863a49cc822cf91077d788917
Yendi Lestrade <jorandradefig@gmail.com>
2020-09-07 23:16:39 -0500

    Esperemos que ahora si funcione

---
+++ media.R
@@ -0,0 +1,17 @@
+
+Hola bienvenida/o a RStudio
+
+toma un poco de aire, ve por un café y un pan que vamos a empezar a programar.
+
+Ejercicio 1
+Vamos a calcular la media de los siguientes datos: 10, 20, 32, 45, 60, 70, 54, 65 y 89.
+
+el primer paso es:
+  crear un vector que contenga todos los numeros
+
+muestra<-c(10,20,32,45,60,70,54,65,89)
+
+Ahora vamos a calcular la media
+mean(muestra)
+
+Bien hecho!!
